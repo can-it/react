8b1fdb14bfe15a196afcf8c69ee996034a072771
Hai Le <haile.vnm@gmail.com>
2023-11-16 17:45:42 +0700

    test(policy-store): correct and add more tests

--- libs/react/src/lib/contexts/policy-store/index.spec.tsx
+++ libs/react/src/lib/contexts/policy-store/index.spec.tsx
@@ -2,52 +2,31 @@
 import '@testing-library/jest-dom';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { PolicyResolver, PolicyStore, usePolicyDispatch, usePolicyState } from '.';
-import { PolicyState } from '@can-it/types';
+import { PolicyState, Request } from '@can-it/types';
 
 describe('Context', () => {
-  describe('No required policy and comparators', () => {
-    function Consumer() {
-      const { policy } = usePolicyState();
-
-      return !policy && 'policy:empty';
-    }
-
-    beforeEach(() => {
-      render(
-        <PolicyStore><Consumer></Consumer></PolicyStore>
-      )
-    });
-
-    it('should have no policy', () => {
-      expect(screen.getByText(/policy:empty/)).toBeTruthy();
-    })
-
-    it('should have no comparators', () => {
-      expect(screen.getByText('comparators:empty')).toBeTruthy();
-    })
-  })
-
-  describe('Pass policy', () => {
+  describe('Using Default Props', () => {
     const SET_POLICY: PolicyState = { allow: [] };
     function Consumer() {
       const { policy } = usePolicyState();
       const { set, update } = usePolicyDispatch();
 
-      const addDeny: PolicyResolver = (pre?: PolicyState) => {
+      const increaseDeny: PolicyResolver = (pre?: PolicyState) => {
+        const request: Request = ['', ''];
         if (!pre) {
-          return { allow: [], deny: [['', '']] };
+          return { allow: [], deny: [request] };
         }
 
-        return { ...pre, deny: (pre.deny || []).concat(['', '']) };
+        return { ...pre, deny: (pre.deny || []).concat([request]) };
       }
 
       return (<>
         <div>{ !policy && 'policy:empty' || 'policy:presence' }</div>
         <div data-testid="allow-count">{ policy?.allow.length }</div>
-        <div data-testid="deny-count">{ policy?.deny }</div>
+        <div data-testid="deny-count">{ policy?.deny?.length }</div>
         <button data-testid="initial" onClick={() => set(SET_POLICY)}></button>
         <button data-testid="reset" onClick={() => set()}></button>
-        <button data-testid="deny" onClick={() => update(addDeny) }></button>
+        <button data-testid="increase-deny" onClick={() => update(increaseDeny) }></button>
       </>);
     }
 
@@ -60,22 +39,18 @@ describe('Context', () => {
       expect(screen.getByText('policy:empty')).toBeTruthy();
     })
 
-    it('should have no comparators', () => {
-      expect(screen.getByText('comparators:empty')).toBeTruthy();
-    })
-
     it('should able to set policy', () => {
       expect(screen.getByTestId('allow-count')).toHaveTextContent('');
 
       fireEvent.click(screen.getByTestId('initial'));
-      expect(screen.getByTestId('allow-count')).toHaveTextContent('1');
+      expect(screen.getByTestId('allow-count')).toHaveTextContent('0');
     })
 
     it('should able to reset policy', () => {
       expect(screen.getByTestId('allow-count')).toHaveTextContent('');
 
       fireEvent.click(screen.getByTestId('initial'));
-      expect(screen.getByTestId('allow-count')).toHaveTextContent('1');
+      expect(screen.getByTestId('allow-count')).toHaveTextContent('0');
 
       fireEvent.click(screen.getByTestId('reset'));
       expect(screen.getByTestId('allow-count')).toHaveTextContent('');
@@ -84,11 +59,41 @@ describe('Context', () => {
     it('should able to update policy base on previous policy', () => {
       expect(screen.getByTestId('deny-count')).toHaveTextContent('');
 
-      fireEvent.click(screen.getByTestId('deny'));
+      fireEvent.click(screen.getByTestId('increase-deny'));
       expect(screen.getByTestId('deny-count')).toHaveTextContent('1');
 
-      fireEvent.click(screen.getByTestId('deny'));
+      fireEvent.click(screen.getByTestId('increase-deny'));
       expect(screen.getByTestId('deny-count')).toHaveTextContent('2');
+    })
+  })
+
+
+  describe('Using custom props', () => {
+    function Consumer() {
+      const { policy } = usePolicyState();
+
+      return (<>
+        <div data-testid="allow-count">{ policy?.allow.length }</div>
+        <div data-testid="deny-count">{ policy?.deny?.length }</div>
+      </>);
+    }
+
+    beforeEach(() => {
+      const policy: PolicyState = {
+        allow: [['', '']],
+        deny: [
+          ['', ''],
+          ['', '']
+        ]
+      };
+      render(
+        <PolicyStore policy={policy}><Consumer></Consumer></PolicyStore>
+      )
+    });
+
+    it('should able to get the custom props', () => {
+      expect(screen.getByTestId('allow-count')).toHaveTextContent('1')
+      expect(screen.getByTestId('deny-count')).toHaveTextContent('2')
     })
   })
 });
